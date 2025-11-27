a7d09840d665ffd0ed4f100d6a2fe8a4c1e55cd3
Sahil Kumar <Sahilkumar32755@gmail.com>
2025-11-27 21:54:46 +0530

    feat:Added the blog CRUD flow.

--- src/app.js
+++ src/app.js
@@ -17,15 +17,14 @@ app.use(express.urlencoded({ extended: true }));
 
 // Start creating the endpoints:
 
-app.get("/",(req,res)=>{
-    return res.status(200).json({message:"Server Starts Serving."});
-})
-
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Server Starts Serving." });
+});
 
 const paymentRouter = require("./Router/payment.router");
+const blogRouter = require("./Router/blog.router");
 
-app.use("/api/v1",paymentRouter);
-
+app.use("/api/v1", paymentRouter);
+app.use("/api/v1", blogRouter);
 
 module.exports = app;
-
